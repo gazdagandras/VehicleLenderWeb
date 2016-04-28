781090de3ad8d92240fea558834952eee0a759d6
Gazdag András <gazdag.andras@gmail.com>
2016-04-28 18:47:10 +0200

    Load uploaded XML file into site

--- src/java/vehiclelenderweb/UploadXMLBean.java
+++ src/java/vehiclelenderweb/UploadXMLBean.java
@@ -1,8 +1,18 @@
 package vehiclelenderweb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.servlet.http.Part;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -13,6 +23,9 @@ import javax.servlet.http.Part;
 public class UploadXMLBean {
 
     private Part file;
+    
+    @Inject
+    VehicleLenderApplicationBean appBean;
 
     public Part getFile() {
         return file;
@@ -23,7 +36,19 @@ public class UploadXMLBean {
     }
 
     public void upload() {
-        
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file.getInputStream());
+            
+            appBean.getSite().loadVehicles(doc);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(UploadXMLBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UploadXMLBean.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(UploadXMLBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
